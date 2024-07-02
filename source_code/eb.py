0e1d46f541e0303da360e5fb075f939b09574d96
FelixBauer <e11775196@student.tuwien.ac.at>
2024-07-02 15:02:24 +0200

    UPDATE eb.py

--- source_code/eb.py
+++ source_code/eb.py
@@ -6,13 +6,13 @@ def add_eb_equations(m=None):
         m.v_eb_q_heat_in[y, t, s] <= m.v_eb_Q_heat_max[y, s]
     
     def eb_elec_heat(m, y, t, s):
-        m.v_eb_q_heat_in[y, t, s] == m.v_eb_q_elec_in[y, t, s] * m.f_eb_eta
+        m.v_eb_q_heat_in[y, t, s] == m.v_eb_q_elec_in[y, t, s] * m.p_eb_eta
         
     def eb_c_fix(m, y, t, s): #y=0 berücksichtigen fehlt, größer gleich im vergleich zum vorjahr
-        m.v_eb_c_fix(y, s) == (m.v_eb_Q_heat_max[y, s] - m.v_eb_Q_heat_max[y-1, s]) * m.v_eb_c_inv(y, s)
+        m.v_eb_c_fix(y, s) == (m.v_eb_Q_heat_max[y, s] - m.v_eb_Q_heat_max[y-5, s]) * m.p_eb_c_inv(y, s)
         
     def eb_c_var(m, y, t, s): # OPAM = operational and maintanance, förderung?
-        m.v_eb_c_var(y, s) == m.v_eb_q_elec_in[y, t, s] * m.v_c_elec[y, t, s] + m.v_c_opam[y, s]
+        m.v_eb_c_var(y, s) == m.v_eb_q_elec_in[y, t, s] * m.p_c_elec[y, t, s]
 
 def add_eb_variables(m=None):
     
@@ -43,22 +43,22 @@ def add_eb_variables(m=None):
     )
 
 def add_eb_parameters(m=None):
-    m.f_eb_eta = py.Parameter( #Einlesen von Inputs fehlt!
+    m.p_eb_eta = py.Parameter( #Einlesen von Inputs fehlt!
         within = py.NonNegativeReals,
         doc = 'electrical efficiency of the eb'
     )
-    m.v_eb_c_inv =py.Parameter( #Einlesen von Inputs fehlt!
+    m.p_eb_c_inv =py.Parameter( #Einlesen von Inputs fehlt!
         m.set_years * m.set_scenarios,
         within = py.NonNegativeReals,
         doc = 'specific inv cost of eb'
         )
-    m.v_c_elec( #Einlesen von Inputs fehlt!
+    m.p_c_elec( #Einlesen von Inputs fehlt!
         m.set_years * m.set_scenarios * m.set_hours,
         within = py.Reals,
         doc = 'specific electriyity prices year hour and scenario'
         )
-    m.v_c_opam( #Einlesen von Inputs fehlt!
-        m.set_years * m.set_scenarios,
-        within = py.NonNegativeReals,
-        doc = 'Operational and Maintanance cost per year and scenario'
-        )
+    # m.p_c_opam( #Einlesen von Inputs fehlt!
+    #     m.set_years * m.set_scenarios,
+    #     within = py.NonNegativeReals,
+    #     doc = 'Operational and Maintanance cost per year and scenario'
+    #     )
